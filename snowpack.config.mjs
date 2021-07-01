b669163d039d1834766237f7258e0c224731cf91
ryan lee martin <brightblade42@protonmail.com>
2021-07-01 09:18:59 -0600

    added snowpack plugin to workaround issue of production build not importing images

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -5,6 +5,11 @@
 import proxy from 'http2-proxy';
 
 export default  {
+  optimize: {
+      bundle: true,
+      minify: true,
+      target: 'es2018',
+  },
   mount: {
     /* ... */
     "src/SAFEwind.Client/public": "/",
@@ -16,6 +21,7 @@ export default  {
     '@snowpack/plugin-dotenv',
     '@snowpack/plugin-postcss',
     '@snowpack/plugin-typescript',
+    ['./snowplug1.js'],
     '@jadex/snowpack-plugin-tailwindcss-jit' //temp fix for spack/jit issue
   ],
   packageOptions: {
@@ -40,29 +46,39 @@ export default  {
         }
       },
 
+      //TODO: snowpack won't properly proxy our web socket! The dirty workaround I'm using is:
+      // 1. use the Allow CORS chrome extension.
+      // 2. connect direcly to websocket host url in code instead of routing through dev server proxy.
+      //     -- remember to change the address or set a dev/prod switch in code to remove hardcode address.
+      // Another hacky but works production from the fine people at NeedToGetShitDoneANDRunningOutOfTime Inc.
+      //We'll come back to this when snowpack fixes it or I realize I was doing something stupid.
+      /*
       {
-        src: '/socket/*',  //pattern might not be right, need *?
-        upgrade: (req, socket, head) => {
+            src: '/socket/fr/.*',  //pattern might not be right, need *?
+            upgrade: (req, socket, head) => {
 
-            const defaultWSHandler = (err, req, socket, head) => {
-              if (err) {
-                console.error('proxy error', err);
-                socket.destroy();
-              }
-            };
+                const defaultWSHandler = (err, req, socket, head) => {
+                  if (err) {
+                    console.error('proxy error', err);
+                    socket.destroy();
+                  }
+                };
 
-            proxy.ws(
-              req,
-              socket,
-              head,
-              {
-                hostname: 'localhost',
-                port: 8085,
+
+                proxy.ws(
+                  req,
+                  socket,
+                  head,
+                  {
+                    hostname: 'localhost',
+                    port: 8085,
+                  },
+                  defaultWSHandler,
+                );
               },
-              defaultWSHandler,
-            );
-          },
        },
+
+       */
 
   ]
 };
---
+++ snowplug1.js
@@ -0,0 +1,11 @@
+module.exports = function config() {
+    return {
+        name: 'snowpack-config-resolveProxyImports-plugin',
+        config(config) {
+            console.log("SNOW PLUG ONE -- ROGER THAT")
+            setTimeout(() => {
+                config.buildOptions.resolveProxyImports = true
+            })
+        },
+    };
+}
